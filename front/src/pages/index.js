4532a98425001f656f60e7991224f0a0b584f10c
Sergey Lukin <contact@sergeylukin.com>
2021-01-04 17:27:48 +0200

    Replace next with gatsby

---
+++ front/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import logo from "../assets/logo.png"
+import "./index.css"
+
+export default function Home() {
+  return (
+    <div className="center">
+      <img src={logo} alt="Logo" />
+    </div>
+  )
+}
